ae0f78d85f9b2b1a81f1f473faf7d07e3bfc11bc
Rodrigo Soares Ferreira <92071020+Atreus75@users.noreply.github.com>
2025-09-13 18:41:09 -0300

    Add files via upload

--- Blokspell/blocklib.py
+++ Blokspell/blocklib.py
@@ -17,6 +17,9 @@ class Utils:
         top_obj.y = back_obj.height/2 - top_obj.height/2
         return top_obj
 
+    def draw_all(objects=[]):
+        for obj in objects:
+            obj.draw()
 
 class Menu:
     def __init__(self, width=0, height=0):
@@ -29,6 +32,7 @@ class Menu:
         self.background.width = self.main_window.width
         self.background.height = self.main_window.height
 
+    def preset_blocks(self):
         # Pre-configures the little blocks for their background animation
         self.blocks = [
             Sprite('src/I_azul.png'),
@@ -55,13 +59,16 @@ class Menu:
             block.y = self.resolution['y']
         self.last_block_renderization = time()
 
+    def preset_play_btn(self):
         # Play button configuration
         self.play_btn = GameImage('src/play_button1.png')
         self.play_btn = Utils.centralize_over_object(self.play_btn, self.main_window)
-        self.play_btn.y = self.play_btn.y+75
+        self.play_btn.y = self.play_btn.y + 80
         self.last_play_btn_animation = time();
+        self.play_btn.height -= self.play_btn.height/2.7
 
-        # Mage animation
+    def preset_mage(self):
+        # Menu Mage
         self.mage_head = GameImage('src/cabeça_mago.png')
         self.mage_head.image = pygame.transform.scale(self.mage_head.image, (self.resolution['x']/100*60, self.resolution['y']/100*80))
         self.mage_head.x = self.resolution['x']/2 - int(self.mage_head.image.get_width()/2)
@@ -69,19 +76,23 @@ class Menu:
 
         self.mage_hand1 = GameImage('src/mao1.png')
         self.mage_hand1.x = self.play_btn.x - int(self.play_btn.width)/2 + 75
-        self.mage_hand1.y = self.play_btn.y - int(self.play_btn.height)/2 - 10
+        self.mage_hand1.y = self.play_btn.y - (int(self.play_btn.height) - 30)
 
         self.mage_hand2 = GameImage('src/mao2.png')
         self.mage_hand2.x = self.mage_hand1.x - 55
-        self.mage_hand2.y = self.mage_hand1.y - 10
+        self.mage_hand2.y = self.mage_hand1.y - 15
+        
+        self.origin_mage_hand1_y = self.mage_hand1.y
+        self.origin_mage_hand2_y = self.mage_hand2.y
+        self.last_mage_animation = time()
+        self.hand_vely = 1
 
-    def falling_blocks_anim(self, interval_secs):
+    def animate_falling_blocks(self, interval_secs):
         block = choice(self.blocks)
-
         if time()-self.last_block_renderization >= interval_secs:
             self.last_block_renderization = time()
-            if block.y >= self.resolution['y']:
-                
+
+            if block.y >= self.resolution['y']:    
                 if block.x in self.block_ocupied_positions:
                     self.block_ocupied_positions.remove(block.x)
                 
@@ -91,30 +102,29 @@ class Menu:
                 while pos_x in self.block_ocupied_positions:
                     pos_x = choice(list(first_space)+list(second_space))
                 block.x = pos_x
-                block.y = randint(5, 15)-block.height
+                block.y = randint(-10, -5) - block.height
                 self.block_ocupied_positions.append(pos_x)
-        block.move_y(4)
+        for block in self.blocks:
+            block.move_y(2)
 
-    def enemies_anim(self, interval_secs):
-        enemie = choice(self.enemies)
-        if time()-self.last_enemie_renderization >= interval_secs:
-            self.last_enemie_renderization = time()
-            if enemie.x <= -1:
-                if enemie.x in self.enemie_ocupied_positions:
-                    self.enemie_ocupied_positions.remove(enemie.x)
+    def animate_mage_hands(self, interval_secs):
+        reached_top = self.mage_hand1.y >= self.origin_mage_hand1_y
+        reached_bottom = self.mage_hand1.y+10 <= self.origin_mage_hand1_y - 20
 
-                pos_x = choice(list(range(self.resolution['x']+1, self.resolution['x']+10)))
-                while pos_x in self.enemie_ocupied_positions:
-                    pos_x = choice(list(range(self.resolution['x']+1, self.resolution['x']+10)))
-                enemie.x = pos_x
-                self.enemie_ocupied_positions.append(pos_x)
-        enemie.move_x(-2)
+        if time()-self.last_mage_animation >= interval_secs:
+            self.last_mage_animation = time()
+            if reached_top or reached_bottom:
+                self.hand_vely *= -1
+            self.mage_hand1.y += self.hand_vely
+            self.mage_hand2.y += self.hand_vely * -1
 
-    def play_btn_anim(self, interval_secs):
-        if time() - self.last_play_btn_animation >= interval_secs:
+    def animate_play_btn(self, interval_secs):
+        if time() - self.last_play_btn_animation >= interval_secs and interval_secs != -1:
             self.play_btn.x = self.play_btn.x * -1
             self.last_play_btn_animation = time()
-            
+        elif interval_secs == -1:
+            self.play_btn.x = self.play_btn.x if self.play_btn.x > 0 else self.play_btn.x * -1
 
     def close(self):
         self.main_window.close()
+
--- Blokspell/main.py
+++ Blokspell/main.py
@@ -1,29 +1,52 @@
-from blocklib import Utils, Menu
+from blocklib import Menu, Utils
+from PPlay.mouse import *
+from time import sleep
 
-# Variáveis GLobais
+# Structural variables
 resolution = {'x' : 1280, 'y' : 720}
+player_mouse = Mouse()
+in_menu = True
 
-
-
+# Menu Definitions
 menu = Menu(1200, 720)
 menu.main_window.set_title('Blockspell')
+menu.preset_blocks()
+menu.preset_play_btn()
+menu.preset_mage()
+
+play_btn_anim_interval = 0.7
+blocks_anim_interval = 0.05
+mage_anim_interval = 0.05
+
 
 # Menu Loop
 while True:
-    menu.background.draw()
-    
-    # Animations
-    menu.falling_blocks_anim(1)
-    for block in menu.blocks:
-        block.draw()
+    if in_menu:
+        # Verifications
+        if player_mouse.is_over_object(menu.play_btn):
+            play_btn_anim_interval = -1
+            mage_anim_interval = 0.005
+            
+            if player_mouse.is_button_pressed(1):
+                menu.mage_hand1.y = menu.origin_mage_hand1_y
+                menu.mage_hand2.y = menu.origin_mage_hand2_y
+                
 
-#    menu.enemies_anim(2)
- #   for enemie in menu.enemies:
-  #      enemie.draw()
-    
-    menu.play_btn_anim(0.7)
-    menu.play_btn.draw()
-    menu.mage_head.draw()
-    menu.mage_hand1.draw()
-    menu.mage_hand2.draw()
-    menu.main_window.update()
+        else:
+            play_btn_anim_interval = 0.7
+            mage_anim_interval = 0.05
+
+        # Animations
+        menu.animate_falling_blocks(blocks_anim_interval)
+#        menu.animate_play_btn(play_btn_anim_interval)
+        menu.animate_mage_hands(mage_anim_interval)
+
+        # Draws in visual layer order (back to front)
+        menu.background.draw()
+        
+        # Draws all blocks
+        for block in menu.blocks:
+            block.draw()
+
+        Utils.draw_all([menu.play_btn, menu.mage_head, menu.mage_hand1, menu.mage_hand2])
+        menu.main_window.update()
